25cd4f8469577f04c573c92dc799d93ba97a8858
tingkai <tingkai@csie.io>
2019-08-31 23:06:52 +0800

    remove 'stop docker container' command

--- backend/build.sh
+++ backend/build.sh
@@ -10,6 +10,13 @@ if ! [ -x "$(command -v git)" ]; then
 	exit 1
 fi
 
+if ! [ -x "$(command -v mvn)" ]; then
+	echo 'Error: maven is not installed.' >&2
+	exit 1
+fi
+
+mvn clean install package
+
 commit_date="$(git show -s --format=%ci --date=short)"
 version=v"$(git log -1 --pretty=format:%h)"-"$(echo $commit_date | cut -d' ' -f1 | tr "-" .)"
 
@@ -17,6 +24,6 @@ docker build . --rm --tag=tingkai/prototype --tag=tingkai/prototype:$version
 docker rmi "$(docker images -f 'dangling=true' -q)"
 docker push tingkai/prototype:latest
 docker push tingkai/prototype:$version
-docker image rm tingkai/prototype:$version
+docker image rm tingkai/prototype:$version tingkai/prototype:latest
 
 exit 0
