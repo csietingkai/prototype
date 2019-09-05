d77abd20443db3853cc5d9c21ddbe8a8e468f5b1
tingkai <tingkai@csie.io>
2019-09-05 22:12:32 +0800

    set proxy for axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			users: []
+		}
+	}
+
+	componentDidMount() {
+    axios.get('/api/user/getAll')
+      .then(res => {
+        this.setState({ users: res.data });
+      })
+  }
 
 	render() {
+		const items = this.state.users.map(user => <li>{user.account}</li>);
+
 		return (
 			<div>
 				<h1>Hello World!</h1>
+				<ul>{items}</ul>
 			</div>
 		)
 	}
