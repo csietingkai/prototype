b13089b5d7b817ce5c6b7fc9551007253aeab661
tingkai <tingkai@csie.io>
2019-09-20 14:13:12 +0800

    axios with token success

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,32 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			users: []
+			items: []
 		}
 	}
 
 	componentDidMount() {
-    axios.get('/api/user/getAll')
-      .then(res => {
-        this.setState({ users: res.data });
-      })
-  }
+		const params= {
+			username: 'tingkai',
+			password: 'htkkoeoh'
+		}
+		axios.post('/api/login', null, {params}).then((response) => {
+			console.log(response);
+			const headers = {
+				'Access-Control-Allow-Headers': 'x-access-token',
+				'X-Auth-Token': response.data.tokenString
+			};
+			axios.get('/api/item/getAll', {headers}).then(items => {
+				console.log(items);
+				this.setState({
+					items: items.data
+				});
+			})
+		})
+	}
 
 	render() {
-		const items = this.state.users.map((user, idx) => <li key={idx}>{user.account}</li>);
+		const items = this.state.items.map((item, idx) => <li key={idx}>{item.name} - ${item.price}</li>);
 
 		return (
 			<div>
