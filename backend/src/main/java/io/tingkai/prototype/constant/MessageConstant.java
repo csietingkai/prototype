17c07897bd4c50208ff8d510167f6f564bf42920
TingKai_Hu <tingkai.hu@hy-tech.com.tw>
2020-01-16 18:03:08 +0800

    add comment

--- backend/src/main/java/io/tingkai/prototype/constant/MessageConstant.java
+++ backend/src/main/java/io/tingkai/prototype/constant/MessageConstant.java
@@ -1,5 +1,10 @@
 package io.tingkai.prototype.constant;
 
+/**
+ * messages used for responses
+ * 
+ * @author tingkai
+ */
 public class MessageConstant {
 
 	public static final String ERROR_MSG_USER_NOT_FOUND = "User not found";
