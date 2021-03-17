de786e6a4bd7f47078291976ba77d9cd1fbd4725
tingkai <tingkai@csie.io>
2021-03-17 14:33:10 +0800

    change CrudRepository to JpaRepository

--- backend/src/main/java/io/tingkai/prototype/dao/ItemDao.java
+++ backend/src/main/java/io/tingkai/prototype/dao/ItemDao.java
@@ -3,18 +3,18 @@ package io.tingkai.prototype.dao;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import io.tingkai.prototype.entity.Item;
 
 /**
- * CrudRepository for sql database table 'item'
+ * JpaRepository for sql database table 'item'
  * 
  * @author tingkai
  */
 @Repository
-public interface ItemDao extends CrudRepository<Item, UUID> {
+public interface ItemDao extends JpaRepository<Item, UUID> {
 
 	public Optional<Item> findByName(String name);
 }
--- backend/src/main/java/io/tingkai/prototype/dao/UserDao.java
+++ backend/src/main/java/io/tingkai/prototype/dao/UserDao.java
@@ -3,19 +3,19 @@ package io.tingkai.prototype.dao;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import io.tingkai.prototype.entity.User;
 import io.tingkai.prototype.enumeration.Role;
 
 /**
- * CrudRepository for sql database table 'users'
+ * JpaRepository for sql database table 'users'
  * 
  * @author tingkai
  */
 @Repository
-public interface UserDao extends CrudRepository<User, UUID> {
+public interface UserDao extends JpaRepository<User, UUID> {
 
 	public Optional<User> findByName(String name);
 
